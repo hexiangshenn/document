c5cf009109b4f29703efca5511e7e5552c9baaa4
hexiangshenn <hexiangshenn@163.com>
2021-01-27 16:24:58 +0800

    规格组

--- mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/SpecificationServiceImpl.java
+++ mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/SpecificationServiceImpl.java
@@ -7,6 +7,8 @@ import com.baidu.shop.entity.SpecGroupEntity;
 import com.baidu.shop.mapper.SpecGroupMapper;
 import com.baidu.shop.service.SpecificationService;
 import com.baidu.shop.utils.BaiduBeanUtil;
+import com.google.gson.JsonObject;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.mapper.entity.Example;
 
@@ -32,5 +34,27 @@ public class SpecificationServiceImpl extends BaseApiService implements Specific
         example.createCriteria().andEqualTo("cid", BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class).getCid());
         List<SpecGroupEntity> list = specGroupMapper.selectByExample(example);
         return this.setResultSuccess(list);
+    }
+
+    @Transactional
+    @Override//新增
+    public Result<JsonObject> saveSpecGroup(SpecGroupDTO specGroupDTO) {
+        specGroupMapper.insertSelective(BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class));
+        return this.setResultSuccess();
+    }
+
+    @Transactional
+    @Override//修改
+    public Result<JsonObject> updateSpecGroup(SpecGroupDTO specGroupDTO) {
+        specGroupMapper.updateByPrimaryKeySelective(BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class));
+        return this.setResultSuccess();
+    }
+
+    @Transactional
+    @Override//删除
+    public Result<JsonObject> deleteSpecGroup(Integer id) {
+        //删除
+        specGroupMapper.deleteByPrimaryKey(id);
+        return this.setResultSuccess();
     }
 }
