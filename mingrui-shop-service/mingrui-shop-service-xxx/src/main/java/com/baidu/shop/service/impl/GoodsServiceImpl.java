f6cfc71944a3d765026e5599e99cfbe6cf67dc74
hexiangshenn <hexiangshenn@163.com>
2021-02-03 19:55:42 +0800

    商品管理

--- mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/GoodsServiceImpl.java
+++ mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/GoodsServiceImpl.java
@@ -2,7 +2,9 @@ package com.baidu.shop.service.impl;
 
 import com.baidu.shop.base.BaseApiService;
 import com.baidu.shop.base.Result;
+import com.baidu.shop.dto.SkuDTO;
 import com.baidu.shop.dto.SpuDTO;
+import com.baidu.shop.dto.SpuDetailDTO;
 import com.baidu.shop.entity.*;
 import com.baidu.shop.mapper.*;
 import com.baidu.shop.service.GoodsService;
@@ -34,39 +36,167 @@ import java.util.stream.Collectors;
  **/
 @RestController
 public class GoodsServiceImpl extends BaseApiService implements GoodsService {
-
     @Resource
     private SpuMapper spuMapper;
     @Resource
     private CategoryMapper categoryMapper;
     @Resource
     private BrandMapper brandMapper;
+    @Resource
+    private SpuDetailMapper spuDetailMapper;
+    @Resource
+    private SkuMapper skuMapper;
+    @Resource
+    private StockMapper stockMapper;
+
+    //提取重复的 删除sku stock 代码
+    private void deleteSkuAndStock (Integer spuId){
+        Example example = new Example(SpuEntity.class);
+        example.createCriteria().andEqualTo("spuId",spuId);
+        List<SkuEntity> skuEntities = skuMapper.selectByExample(example);
+
+        List<Long> skuIdList = skuEntities.stream().map(spuEntity -> spuEntity.getId()).collect(Collectors.toList());
+        skuMapper.deleteByIdList(skuIdList);
+        stockMapper.deleteByIdList(skuIdList);
+    }
+    //提取重复的 新增sku stock 代码
+    private void insertSkuAndStock (SpuDTO spuDTO,Integer spuId,Date date){
+        List<SkuDTO> skuList = spuDTO.getSkus();
+        skuList.stream().forEach(skuDTO -> {
+            SkuEntity skuEntity = BaiduBeanUtil.copyProperties(skuDTO, SkuEntity.class);
+            skuEntity.setSpuId(spuId);
+            skuEntity.setCreateTime(date);
+            skuEntity.setLastUpdateTime(date);
+            skuMapper.insertSelective(skuEntity);
+
+            StockEntity stockEntity = new StockEntity();
+            stockEntity.setSkuId(skuEntity.getId());
+            stockEntity.setStock(skuDTO.getStock());
+            stockMapper.insertSelective(stockEntity);
+        });
+    }
+
+
+    @Transactional
+    @Override//删除
+    public Result<JsonObject> deleteGoods(Integer spuId) {
+        spuMapper.deleteByPrimaryKey(spuId);
+        spuDetailMapper.deleteByPrimaryKey(spuId);
+
+        this.deleteSkuAndStock(spuId);
+
+        return setResultSuccess();
+    }
+
+    //修改
+    @Override
+    @Transactional
+    public Result<JsonObject> editGoods(SpuDTO spuDTO) {
+        final Date date = new Date();
+        SpuEntity spuEntity = BaiduBeanUtil.copyProperties(spuDTO, SpuEntity.class);
+        spuEntity.setLastUpdateTime(date);
+        spuMapper.updateByPrimaryKeySelective(spuEntity);
+
+        spuDetailMapper.updateByPrimaryKeySelective(BaiduBeanUtil.copyProperties(spuDTO.getSpuDetail(),SpuDetailEntity.class));
+
+        //删除
+        Example example = new Example(SkuEntity.class);
+        example.createCriteria().andEqualTo("spuId",spuEntity.getId());
+        List<SkuEntity> skuEntities = skuMapper.selectByExample(example);
+
+        List<Long> skuIdList = skuEntities.stream().map(skuEntity -> skuEntity.getId()).collect(Collectors.toList());
+        skuMapper.deleteByIdList(skuIdList);
+        stockMapper.deleteByIdList(skuIdList);
+
+        this.insertSkuAndStock(spuDTO,spuEntity.getId(),date);
+
+        return this.setResultSuccess();
+    }
+
+    @Override
+    public Result<SpuDetailEntity> getSpuDetailBySpu(Integer spuId) {
+        SpuDetailEntity spuDetailEntity = spuDetailMapper.selectByPrimaryKey(spuId);
+        return this.setResultSuccess(spuDetailEntity);
+    }
+
+    @Override
+    public Result<SkuDTO> getSkuBySpuId(Integer spuId) {
+        List<SkuDTO> list = skuMapper.selectSkuAndStockBySpuId(spuId);
+
+        return this.setResultSuccess(list);
+    }
+
+
+    //新增!!!!!!!
+    @Override
+    @Transactional
+    public Result<JsonObject> saveGoods(SpuDTO spuDTO) {
+        System.out.println(spuDTO);
+        final Date date = new Date();
+        //新增spu
+        SpuEntity spuEntity = BaiduBeanUtil.copyProperties(spuDTO, SpuEntity.class);
+        spuEntity.setSaleable(1);
+        spuEntity.setValid(1);
+        spuEntity.setCreateTime(date);
+        spuEntity.setLastUpdateTime(date);
+        spuMapper.insertSelective(spuEntity);
+
+        //新增大字段
+        SpuDetailDTO spuDetail = spuDTO.getSpuDetail();
+        SpuDetailEntity spuDetailEntity = BaiduBeanUtil.copyProperties(spuDetail, SpuDetailEntity.class);
+        spuDetailEntity.setSpuId(spuEntity.getId());
+        spuDetailMapper.insertSelective(spuDetailEntity);
+
+        //新增sku,sku可能时多条数据
+        this.insertSkuAndStock(spuDTO,spuEntity.getId(),date);
+        return this.setResultSuccess();
+    }
 
     @Override
     public Result<List<SpuDTO>> getSpuInfo(SpuDTO spuDTO) {
-        SpuEntity spuEntity = BaiduBeanUtil.copyProperties(spuDTO, SpuEntity.class);
-
-        if(!StringUtils.isEmpty(spuDTO.getSort())){
-            PageHelper.orderBy(spuDTO.getOrderBy());
-        }
 
         if (ObjectUtil.isNotNull(spuDTO.getPage()) && ObjectUtil.isNotNull(spuDTO.getRows())){
             PageHelper.startPage(spuDTO.getPage(),spuDTO.getRows());
         }
-
+        if(!StringUtils.isEmpty(spuDTO.getSort()) && !StringUtils.isEmpty(spuDTO.getOrder())){
+            PageHelper.orderBy(spuDTO.getOrderBy());
+        }
         Example example = new Example(SpuEntity.class);
         Example.Criteria criteria = example.createCriteria();
 
-        //判断前台传来的
-        if (ObjectUtil.isNotNull(spuEntity.getSaleable()) && spuEntity.getSaleable() != 2){
-            criteria.andEqualTo("saleable",spuEntity.getSaleable());
+        if (ObjectUtil.isNotNull(spuDTO.getSaleable()) && spuDTO.getSaleable() != 2){
+            criteria.andEqualTo("saleable",spuDTO.getSaleable());
         }
-        if (ObjectUtil.isNotNull(spuEntity.getTitle())){
-            criteria.andLike("title","%"+spuEntity.getTitle()+"%");
+        if (!StringUtils.isEmpty(spuDTO.getTitle())){
+            criteria.andEqualTo("title","%"+spuDTO.getTitle()+"%");
         }
+        List<SpuEntity> spuEntities = spuMapper.selectByExample(example);
 
-        List<SpuEntity> list = spuMapper.selectByExample(example);
-        PageInfo<SpuEntity> pageInfo = new PageInfo<>(list);
-        return this.setResultSuccess(pageInfo);
+        List<SpuDTO> spuDTOList = spuEntities.stream().map(spuEntity -> {
+            SpuDTO spuDTO1 = BaiduBeanUtil.copyProperties(spuEntity,SpuDTO.class);
+
+            List<CategoryEntity> categoryEntities = categoryMapper.selectByIdList(Arrays.asList(spuEntity.getCid1(), spuEntity.getCid2(), spuEntity.getCid3()));
+
+            String categoryName = categoryEntities.stream().map(categoryEntity -> categoryEntity.getName()).collect(Collectors.joining("/"));
+            spuDTO1.setCategoryName(categoryName);
+
+            BrandEntity brandEntity = brandMapper.selectByPrimaryKey(spuEntity.getBrandId());
+            spuDTO1.setBrandName(brandEntity.getName());
+            return spuDTO1;
+        }).collect(Collectors.toList());
+
+        PageInfo<SpuEntity> pageInfo = new PageInfo<>(spuEntities);
+        return this.setResult(HTTPStatus.OK,pageInfo.getTotal() + "",spuDTOList);
+    }
+
+    //上架下架
+    @Transactional
+    @Override
+    public Result<JsonObject> upAndDown(SpuDTO spuDTO) {
+        SpuEntity spuEntity = BaiduBeanUtil.copyProperties(spuDTO, SpuEntity.class);
+        spuEntity.setId(spuDTO.getId());
+        spuEntity.setSaleable(spuDTO.getSaleable());
+        spuMapper.updateByPrimaryKeySelective(spuEntity);
+        return this.setResultSuccess();
     }
 }
